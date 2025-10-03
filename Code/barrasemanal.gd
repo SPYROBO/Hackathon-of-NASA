10b46dce99628d902ebf6f1fc0d428f9e1984b2a
Thiago <caponethiagomanuel@gmail.com>
2025-10-02 22:43:23 -0300

    Semanas y dias

--- Code/barrasemanal.gd
+++ Code/barrasemanal.gd
@@ -8,13 +8,28 @@ const DIAS_DE_LA_SEMANA: Array[String] = [
 	"Sábado",
     "Domingo"
 ]
+const Semana: Array[String] = [
+	"Semana 1",
+	"Semana 2",
+	"Semana 3",
+	"Semana 4"
+]
 @export var i=0
+@export var m=0
 
 
 
 func _on_logica_dia_actualizado(_dia_actual: Variant) -> void:
 	if i==6:
-		print("tiempo termindo")
+		print("Semana terminada")
+		i=0
+		$LabelDia.set_text(DIAS_DE_LA_SEMANA[i])
+		if m==3:
+			print("Juego terminado")
+		else:
+			m=m+1
+			$LabelSemana.set_text(Semana[m])
+			
 	else:
 		i=i+1
 		$LabelDia.set_text(DIAS_DE_LA_SEMANA[i])
